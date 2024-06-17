8d02ac6eaad053546a3badf211472c764b7ec939
Stiven Castellon <stiven.scd13@gmail.com>
2024-06-16 21:58:53 -0400

    block

--- objectivec/main.m
+++ objectivec/main.m
@@ -10,6 +10,18 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
+        
+        void (^simpleBlock)(void) = ^ {
+            NSLog(@"simple block");
+        };
+        simpleBlock();
+        
+        double (^sumTwoValues) (double a, double b) = ^(double a, double b){
+            return a+b;
+        };
+        double sum = sumTwoValues(10,20);
+        NSLog(@"sum: %f", sum );
+        
         char *myCharacter[] = {"Juan", "Carlos", "Maria"};
         qsort_b(myCharacter, 3, sizeof(char *), ^(const void *l, const void *r){
             char *left = *(char **) l;
